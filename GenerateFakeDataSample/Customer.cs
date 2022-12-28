b0c05f134ee31494e0a739b19612614ddf2ad91b
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2022-12-28 09:18:56 +0330

    Add project files.

---
+++ GenerateFakeDataSample/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace GenerateFakeDataSample
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string AddressLine { get; set; }
+
+        public string City { get; set; }
+
+        public string PostCode { get; set; }
+
+        public string Country { get; set; }
+    }
+}
---
+++ GenerateFakeDataSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using GenerateFakeDataSample;
+using System.Text.Json;
+
+Randomizer.Seed = new Random(10);
+
+var customerData = new Faker<Customer>()
+
+    .RuleFor(x => x.Name, x => x.Person.FullName)
+    .RuleFor(x => x.Email, x => x.Person.Email)
+    .RuleFor(x => x.Phone, x => x.Person.Phone)
+    .RuleFor(x => x.AddressLine, x => x.Address.StreetAddress())
+    .RuleFor(x => x.City, x => x.Address.City())
+    .RuleFor(x => x.Country, x => x.Address.Country())
+    .RuleFor(x => x.PostCode, x => x.Address.ZipCode());
+
+for (int i = 0; i < 10; i++)
+{
+    var text = JsonSerializer.Serialize(customerData.Generate());
+    Console.WriteLine(text);
+}
